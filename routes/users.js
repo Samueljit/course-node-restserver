bca9b84cec784469a38a336e3b948c68e8fc933d
Samuel Infante <samueljit@gmail.com>
2022-01-21 23:50:03 -0300

    Verificando si funciona

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 const {
         validateFields,
         validateJWT,
-        adminRole,
+        // adminRole,
         hasARole
  } = require('../middlewares')
  
